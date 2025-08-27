2f47e6b506a67dd865c71c0868cb9b0d5c0652c6
kawser-hamid-bappy <bappydu2015@gmail.com>
2025-08-27 13:46:31 +0600

    Call Button funtaionality added

--- main.js
+++ main.js
@@ -14,31 +14,31 @@ for (let card of singleCard) {
   });
 
   // call button action here
-  // const callBtn = card.children[5].children[1];
-  // const callName = card.children[1].innerText;
-  // const dialNumber = card.children[3].innerText;
-  // callBtn.addEventListener("click", function () {
-  //   let coinCount = Number(getElement("star").innerText);
-  //   getElement("star").innerText = coinCount - 20;
-  //   if (coinCount < 20) {
-  //     alert(`You do not have sufficent coin. you have to have minimum 20 coin to call`);
-  //     getElement("star").innerText = 0;
-  //     return;
-  //   }
-  //   alert(`Calling ${callName} ${dialNumber}...`);
-  //   const newDiv = document.createElement("div");
-  //   const historyDiv = getElement("history");
-  //   newDiv.innerHTML = `<div class="flex justify-between bg-[#FAFAFA] my-2 p-2 rounded-lg">
-  //                     <div>
-  //                       <h3 class="font-bold mb-1">${callName}</h3>
-  //                       <p>${dialNumber}</p>
-  //                     </div>
-  //                     <div>
-  //                       <p><span>${new Date().toLocaleTimeString()}</span></p>
-  //                     </div>
-  //                   </div>`;
-  //   historyDiv.append(newDiv);
-  // });
+  const callBtn = card.children[5].children[1];
+  const callName = card.children[1].innerText;
+  const dialNumber = card.children[3].innerText;
+  callBtn.addEventListener("click", function () {
+    let coinCount = Number(getElement("star").innerText);
+    getElement("star").innerText = coinCount - 20;
+    if (coinCount < 20) {
+      alert(`You do not have sufficent coin. you have to have minimum 20 coin to call`);
+      getElement("star").innerText = 0;
+      return;
+    }
+    alert(`Calling ${callName} ${dialNumber}...`);
+    const newDiv = document.createElement("div");
+    const historyDiv = getElement("history");
+    newDiv.innerHTML = `<div class="flex justify-between bg-[#FAFAFA] my-2 p-2 rounded-lg">
+                      <div>
+                        <h3 class="font-bold mb-1">${callName}</h3>
+                        <p>${dialNumber}</p>
+                      </div>
+                      <div>
+                        <p><span>${new Date().toLocaleTimeString()}</span></p>
+                      </div>
+                    </div>`;
+    historyDiv.append(newDiv);
+  });
 
   // Copy text and past anywhere
   // const copyNumber = card.children[3].innerText;
